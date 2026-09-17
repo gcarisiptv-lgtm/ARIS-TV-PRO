0597cc336a9107023ceb161503701d23463036b0
gcarisiptv-lgtm <gcaris.iptv@gmail.com>
2026-09-17 15:17:44 +0100

    Update ApiConfig.kt

--- ApiConfig.kt
+++ ApiConfig.kt
@@ -1,6 +1,6 @@
 package com.gcaforce.iptvpro
+
 object ApiConfig {
-    // Emulator: http://10.0.2.2:8080/
-    // Physical Android TV: replace with your backend LAN/public HTTPS address.
-    const val BASE_URL = "http://10.0.2.2:8080/"
+    // Backend public ARIS IPTV
+    const val BASE_URL = "https://aris-iptv.onrender.com/"
 }
